0ad90ed7a501d95426e65ae3823c55adc4f3ace4
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2022-10-31 14:57:10 +0600

    -

--- components/senderBlock/senderBlock.jsx
+++ components/senderBlock/senderBlock.jsx
@@ -7,7 +7,8 @@ const SenderBlock = () => {
             <p>Полезные советы и персональный предложения</p>
         </div>
         <div className={styles.search}>
-            <input type="text" name="" placeholder={"Ваш email"} required=""/>
+            <input type="text" name="" required=""/>
+            <label>Ваш email</label>
             <svg width="40" height="36" viewBox="0 0 40 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g filter="url(#filter0_d_33_31)">
                     <path d="M9.9844 12.6279L27.5907 15.0332L9.84697 16.0487L4.50706 26.6656C4.16014 27.3553 4.84811 28.1088 5.56642 27.8258L35.3192 16.1073C35.9998 15.8392 36.0372 14.8899 35.3797 14.5691L6.64118 0.548935C5.9473 0.210421 5.20227 0.907506 5.49388 1.62239L9.9844 12.6279Z" fill="#DADADA"/>
