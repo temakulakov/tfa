ca2556100f2b86db72a9bed4a45dce9cc5ab7b36
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2022-11-01 14:14:10 +0600

    -

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,9 +1,10 @@
 import styles from "./card.module.scss";
 import Image from "next/image";
+import {useState} from "react";
 
 const Card = (props) => {
+    const [visiblePopup, setVisiblePopup] = useState(false);
     return <div className={styles.root} >
-
       <Image style={{width: "100%", height: "100%"}} src={`${props.background}`} alt={"props.children"}  width={300} height={300}/>
        <div className={styles.shadow}></div>
         <p>{props.children}</p>
